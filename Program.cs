9379246f10105af7bb98a2c028cf20c29f6776d0
~ Lydford (1000119721) <LYDFED1@student.op.ac.nz>
2023-09-19 11:38:13 +1200

    This is problematic added method for jumble

--- Program.cs
+++ Program.cs
@@ -10,34 +10,20 @@
 
         public static string Jumble() 
         {
-            return
+            Random rand = new Random();
+            String beanz = "hehehe";
+            char temp;
+            char[] letter = beanz.ToCharArray();
+            int numSwap1 = rand.Next(0,beanz.Length);
+            int numSwap2 = rand.Next(0, beanz.Length);
+            String finalWords = new string(letter);
+            return finalWords;
         }
 
-        public static void user(string answer)
+        public static void user()
 
         {
-            //sets up values and takes a guess from the user
-            string userguess, temp;
-            Console.WriteLine("Please enter your guess:\t");
-            userguess = Console.ReadLine();
-
-            //This part of the code is for when the user guesses correctly.
-            if (userguess == answer)
-            {
-                Console.WriteLine($"Your guess was {userguess}... Your guess was correct. Well done!!!");
-            }
-
-            //This part of the code is for when the user guesses incorrectly.
-            else if (userguess != answer) 
-            {
-                Console.WriteLine($"Your guess was {userguess}. That was incorrect. The correct answer was {answer}. Better luck next time...");
-            }
-
-            //This part of the code is for a stalemate if it were to somehow occur.
-            else
-            {
-                Console.WriteLine("I do not understand how you have got here. I am impressed none the less.");
-            }
+               
         }
 
         static void Main(string[] args)
