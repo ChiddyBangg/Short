3412c3bb99f448c76b3548cf97fdc1443fd7ed3f
~ Hibbs (1000106951) <HIBBLR1@student.op.ac.nz>
2023-09-19 11:32:43 +1200

    this is the user guess and answer check complete

--- Program.cs
+++ Program.cs
@@ -13,10 +13,31 @@
             return
         }
 
-        public static void user()
+        public static void user(string answer)
 
         {
-               
+            //sets up values and takes a guess from the user
+            string userguess, temp;
+            Console.WriteLine("Please enter your guess:\t");
+            userguess = Console.ReadLine();
+
+            //This part of the code is for when the user guesses correctly.
+            if (userguess == answer)
+            {
+                Console.WriteLine($"Your guess was {userguess}... Your guess was correct. Well done!!!");
+            }
+
+            //This part of the code is for when the user guesses incorrectly.
+            else if (userguess != answer) 
+            {
+                Console.WriteLine($"Your guess was {userguess}. That was incorrect. The correct answer was {answer}. Better luck next time...");
+            }
+
+            //This part of the code is for a stalemate if it were to somehow occur.
+            else
+            {
+                Console.WriteLine("I do not understand how you have got here. I am impressed none the less.");
+            }
         }
 
         static void Main(string[] args)
